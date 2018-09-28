77072e3a31f7a1bb74f34498aef8879913aaffa9
fatchur <fatchur.rahman1@gmail.com>
2018-09-29 00:52:24 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='feature_engineering_utils',  # Required
+    version='0.0.1',  # Required
+    description='a simple package for helping feature engineering, ex: data handling and visualization trick',  # Required
+    author='Chur Chur',  # Optional
+    packages=["feature_engineering_utils"],  # Required
+)
