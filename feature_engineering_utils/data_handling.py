2bf7094a3fe826bca7df9d4e9557d7b3ec79b73f
fatchur <fatchur.rahman1@gmail.com>
2018-09-29 09:23:03 +0700

    _

--- feature_engineering_utils/data_handling.py
+++ feature_engineering_utils/data_handling.py
@@ -1,4 +1,14 @@
 def binary_based_class_split(class_names, dataframe, feature_col, target_col):
+    """
+	Splitting the value of certain feature based on the class (binary class)
+	Args:
+		class_names:		a list of clas names, ex:['macet', 'lancar']
+		dataframe:		    The dataframe, contains feature and target or class columns
+		feature_col:        The name of the feature column, a string
+		target_col:         The name of target columns, a string
+	Return:
+		A dataframe with the splitted data based on the class value, the class names are used as feature names
+	"""
     
     dict = {}
     for i in class_names:
