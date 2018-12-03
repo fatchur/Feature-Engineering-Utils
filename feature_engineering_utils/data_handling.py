b32e45173be854a11b24ae8591d7227468d2bcb4
fatchur <fatchur.rahman1@gmail.com>
2018-12-03 12:20:01 +0700

    add date handling class

--- feature_engineering_utils/data_handling.py
+++ feature_engineering_utils/data_handling.py
@@ -7,9 +7,9 @@ def binary_based_class_split(class_names, dataframe, feature_col, target_col):
 	Splitting the value of certain feature based on the class (binary class)
 	Args:
 		class_names:		a list of clas names, ex:['macet', 'lancar']
-		dataframe:		    The dataframe, contains feature and target or class columns
-		feature_col:        The name of the feature column, a string
-		target_col:         The name of target columns, a string
+		dataframe:		    a dataframe, contains feature and target or class columns
+		feature_col:        a list of string, contains the name of the feature column
+		target_col:         a string, the name of target columns (the value of this column is 0 or 1)
 	Return:
 		A dataframe with the splitted data based on the class value, the class names are used as feature names
 	"""
@@ -33,7 +33,6 @@ def binary_based_class_split(class_names, dataframe, feature_col, target_col):
     # return a dictionary, with class names as column names
     df = pd.DataFrame({class_names[0]: dict[class_names[0]], class_names[1]: dict[class_names[1]]})
     return df
-
 
 
 def agg_numeric(df, group_var, df_name):
@@ -84,7 +83,6 @@ def agg_numeric(df, group_var, df_name):
     
     # Merge with the counts
     agg = agg.merge(counts, on = group_var, how = 'left')
-    
     return agg
 
 
@@ -125,6 +123,102 @@ def print_dataframe_full(x):
     pd.set_option('display.max_rows', len(x))
     print(x)
     pd.reset_option('display.max_rows')
+
+
+class TimeHandler():
+    def __init__(self):
+        self.day_dict = {'non_kabisat': {'1': 0, '2':31, '3':59, '4':90, '5':120, '6':151,\
+                                         '7': 181, '8':212, '9':243, '10':273, '11':304, '12':334},
+                         'kabisat': {'1': 0, '2':31, '3':60, '4':91, '5':121, '6':152, \
+                                     '7': 182, '8':213, '9':244, '10':274, '11':305, '12':335}}
+        
+        self.indo_holiday_dict = {2016: {1: [1], 2: [8, 29], 3: [9, 25], 4: [], \
+                                    5: [5], 6: [], 7: [1, 2, 3, 4, 5, 6, 7], 8: [17], \
+                                    9: [12], 10: [], 11:[], 12: [12, 25, 26]},
+                             2017: {1: [1, 28], 2: [], 3: [28], 4: [14, 24], \
+                                    5: [1, 11, 23], 6: [1, 21, 22, 23, 24, 25, 26, 27], 7: [], 8: [17], \
+                                    9: [1, 21], 10: [], 11:[], 12: [1, 25, 26]},
+                             2018: {1: [1], 2: [16], 3:[18, 30], 4: [13], \
+                                    5: [1, 10, 29], 6: [10, 11, 12, 13, 14, 15, 16], 7: [], 8: [17, 22], \
+                                    9: [12], 10: [], 11: [20], 12: [1, 25, 26]},
+                             2019: {1: [1], 2: [5], 3: [7], 4: [3, 19], \
+                                    5: [1, 30, 31], 6: [1, 2, 3, 4, 5, 6], 7: [], 8: [17], \
+                                    9: [12], 10: [], 11:[9], 12: [25, 26]}}
+                         
+        self.weekend_start = {2016:[2, 3], 2017:[1, 7], 2018:[6, 7], 2019:[5, 6]}
+
+    def encode_oneyear_YYYYMMDD(self, pandas_column):
+        pandas_column = pandas_column.str.replace("-", "")
+        ## get the year data
+        year = []
+        month = []
+        date = []
+        encoded_day = []
+        for idx, i in enumerate(pandas_column):
+            tmp_year = int(i[:4])
+            tmp_month = int(i[4:6])
+            tmp_date = int(i[6:8])
+            if tmp_year % 4 == 0:
+                flag = 'kabisat'
+            else: flag = 'non_kabisat'
+            total_day = self.day_dict[flag][str(tmp_month)] + tmp_date
+            #add the value
+            year.append(tmp_year)
+            month.append(tmp_month)
+            date.append(tmp_date)
+            encoded_day.append(total_day)
+        return np.array(year), np.array(month), np.array(date), np.array(encoded_day)
+    
+    def encode_oneyear_date(self, year, pandas_month_column, pandas_date_column):
+        encoded_day = []
+        for j, k in zip(pandas_month_column, pandas_date_column):
+            month = int(j)
+            date = int(k)
+            if year % 4 == 0:
+                flag = 'kabisat'
+            else: flag = 'non_kabisat'
+            total_day = self.day_dict[flag][str(month)] + date 
+            #add the value
+            encoded_day.append(total_day)
+        return encoded_day
+
+    def encode_multiple_year_date(self, pandas_year_column, pandas_month_column, pandas_date_column):
+        min_year = pandas_year_column.min()
+        ## get the year data
+        encoded_day = []
+        for i, j, k in zip(pandas_year_column, pandas_month_column, pandas_date_column):
+            year = int(i)
+            month = int(j)
+            date = int(k)
+            if year % 4 == 0:
+                flag = 'kabisat'
+            else: flag = 'non_kabisat'
+            total_day = self.day_dict[flag][str(month)] + date + 365 * (year - min_year)
+            #add the value
+            encoded_day.append(total_day)
+        return encoded_day
+
+    def is_holiday(self, pandas_year_column, pandas_month_column, pandas_date_column):
+        holiday_list = []
+        for i, j, k in zip(pandas_year_column, pandas_month_column, pandas_date_column):
+            year = int(i)
+            month = int(j)
+            date = int(k)
+            is_holiday = int(date in self.indo_holiday_dict[year][month])
+            holiday_list.append(is_holiday)
+        return holiday_list
+
+    def is_weekend(self, pandas_year_column, pandas_encodedDay_oneYear_column):
+        weekends = []
+        for i, j in zip(pandas_year_column, pandas_encodedDay_oneYear_column):
+            year = int(i)
+            encoded_day = int(j)
+            saturday, sunday = self.weekend_start[year]
+            is_saturday = ((encoded_day - saturday)% 7 == 0)
+            is_sunday = ((encoded_day - sunday)% 7 == 0)
+            weekend = is_saturday or is_sunday
+            weekends.append(int(weekend))
+        return weekends
 
 
 
